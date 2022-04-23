aef1c05ec7220a9c61f7ec7b9b90ebf110d9324b
Enes <66560757+devenes@users.noreply.github.com>
2022-04-24 02:35:44 +0300

    Create user-data.sh

---
+++ user-data.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+
+yum update -y
+amazon-linux-extras install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
+-o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+mkdir -p /home/ec2-user/bookstore-api
+TOKEN="****************************************"
+FOLDER="https://$TOKEN@raw.githubusercontent.com/devenes/terraform-bookstore-api/main/"
+curl -s --create-dirs -o "/home/ec2-user/bookstore-api/app.py" -L "$FOLDER"bookstore-api.py
+curl -s --create-dirs -o "/home/ec2-user/bookstore-api/requirements.txt" -L "$FOLDER"requirements.txt
+curl -s --create-dirs -o "/home/ec2-user/bookstore-api/Dockerfile" -L "$FOLDER"Dockerfile
+curl -s --create-dirs -o "/home/ec2-user/bookstore-api/docker-compose.yml" -L "$FOLDER"docker-compose.yml
+cd /home/ec2-user/bookstore-api
+docker build -t devenes/bookstore-api:latest .
+docker-compose up -d
