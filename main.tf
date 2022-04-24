5e830b9ab862e006ad57d1da9912b2dd76e9e7a8
Enes <66560757+devenes@users.noreply.github.com>
2022-04-24 15:38:58 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,23 +29,23 @@ resource "aws_security_group" "tf-docker-sec-gr" {
   }
 
   ingress {
-    protocol    = "tcp"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    protocol    = "tcp"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
-    protocol    = "-1"
     to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
